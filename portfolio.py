f6ea6facdffe11da596515f90c442a1b9ae21c61
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-08-11 15:12:07 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -25,3 +25,19 @@ for stock, price in stock_prices.items():
     print(stock, ":", "$" + str(price))
 
 print("\nEnter 'DONE' when you finish adding stocks.")
+
+
+# Main loop
+while True:
+
+    # Ask for stock name
+    stock_name = input("\nEnter stock name: ").upper()
+
+    # Stop the program
+    if stock_name == "DONE":
+        break
+
+    # Check if stock exists
+    if stock_name not in stock_prices:
+        print("Stock not available. Please choose from the available stocks.")
+        continue
